c073ad54fbadeb086c944dcac077768e4b368ec5
Tama <tamamhuda11@gmail.com>
2025-07-31 03:40:40 +0700

    feat(auth): add annotation for current user

---
+++ src/main/java/com/tamamhuda/minimart/common/annotation/CurrentUser.java
@@ -0,0 +1,13 @@
+package com.tamamhuda.minimart.common.annotation;
+
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@AuthenticationPrincipal
+public @interface CurrentUser {
+}
