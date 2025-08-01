893f02543fbe20443ad5ba0a41126247e4aed9a7
Tama <tamamhuda11@gmail.com>
2025-08-01 17:05:42 +0700

    chore(cleanup): remove unused imports

--- src/main/java/com/tamamhuda/minimart/config/JwtAuthenticationFilter.java
+++ src/main/java/com/tamamhuda/minimart/config/JwtAuthenticationFilter.java
@@ -2,7 +2,6 @@ package com.tamamhuda.minimart.config;
 
 import com.tamamhuda.minimart.application.service.JwtService;
 import com.tamamhuda.minimart.application.service.impl.UserDetailsServiceImpl;
-import com.tamamhuda.minimart.common.exception.UnauthorizedException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -10,13 +9,11 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
-import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.HandlerExceptionResolver;
 
 import java.io.IOException;
